24b12fdbb4297958e2e91c0b4575d42bb315f2b1
Vina del Mar <vina@duoc.acad>
2019-10-10 15:23:46 -0300

    Nueva zona Noticias

--- sol/models.py
+++ sol/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Noticia(models.Model):
+    fecha_Publicacion = models.DateTimeField(blank=True, null=True)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+
+    def publish(self):
+        self.fecha_Publicacion = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
--- sol/views.py
+++ sol/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Noticia
+from django.utils import timezone
 
 def index(request):
-
-    return render(request, 'sol/index.html', {})
+    Noticias = Noticia.objects.filter(fecha_Publicacion__lte=timezone.now()).order_by('-fecha_Publicacion')[:3]
+    return render(request, 'sol/index.html', {"Noticias":Noticias})
 
 def tarifas(request):
 
