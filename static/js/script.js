bce5b55748a90a513d53c0cf9a987b3a0d2f1907
soldelpacifico <niccolepxd@gmail.com>
2019-10-16 15:06:42 +0000

    Arreglos Nav

--- static/js/script.js
+++ static/js/script.js
@@ -2,35 +2,20 @@ $(document).ready(function(){
   $("#despInfo").click(function(){
     if($(window).width()<1100) {
     $("#liCont1").toggleClass("show");
-    if ($("#liCont1").hasClass("show")) {
-      $("#despInfo").css("background","#000000");
-      $("#despInfo a").css("color","#ffffff");
-    }else{
-      $("#despInfo").css("background","#e9e9e9");
-      $("#despInfo a").css("color","#000000");
-    }}
+    $("#despInfo").toggleClass("clicked");
+    }
   });
   $("#despComu").click(function(){
     if($(window).width()<1100) {
     $("#liCont2").toggleClass("show");
-    if ($("#liCont2").hasClass("show")) {
-      $("#despComu").css("background","#000000");
-      $("#despComu a").css("color","#ffffff");
-    }else{
-      $("#despComu").css("background","#e9e9e9");
-      $("#despComu a").css("color","#000000");
-    }}
+    $("#despComu").toggleClass("clicked");
+    }
   });
     $("#despCont").click(function(){
       if($(window).width()<1100) {
       $("#liCont3").toggleClass("show");
-      if ($("#liCont3").hasClass("show")) {
-        $("#despCont").css("background","#000000");
-        $("#despCont a").css("color","#ffffff");
-      }else{
-        $("#despCont").css("background","#e9e9e9");
-        $("#despCont a").css("color","#000000");
-    }}
+      $("#despCont").toggleClass("clicked");
+    }
   });
 });
 
