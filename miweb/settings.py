93a0476068354d5cf1d9da39ce30a2af49112e11
soldelpacifico <niccolepxd@gmail.com>
2019-10-21 02:15:38 +0000

    cambios via web

--- miweb/settings.py
+++ miweb/settings.py
@@ -136,3 +136,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
