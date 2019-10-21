aff1e82d6c5d374c3c5938ca1a827f814715f6f0
Proxyjoshua <joshuaortizz@hotmail.com>
2019-10-21 02:19:29 -0300

    Nuevos redirect login

--- miweb/settings.py
+++ miweb/settings.py
@@ -118,6 +118,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL='preguntas'
+LOGOUT_REDIRECT_URL='login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
