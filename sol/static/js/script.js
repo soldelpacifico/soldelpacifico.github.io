6122ad2a924a3038fdbd453fdd2ccc1e6fc50719
Proxyjoshua <joshuaortizz@hotmail.com>
2019-10-30 21:26:35 -0300

    Nuevo estilo de aviso

--- sol/static/js/script.js
+++ sol/static/js/script.js
@@ -3,29 +3,10 @@ $(document).ready(function(){
     $(".aviso").css("display","none")
   });
 
-  $("#despInfo").click(function(){
-    if($(window).width()<1100) {
-      $("#liCont1").toggleClass("show");
-      $("#despInfo").toggleClass("clicked");
-    }
+  $('.open-popup-link').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
   });
-  $("#despComu").click(function(){
-    if($(window).width()<1100) {
-      $("#liCont2").toggleClass("show");
-      $("#despComu").toggleClass("clicked");
-    }
-  });
-  $("#despCont").click(function(){
-    if($(window).width()<1100) {
-      $("#liCont3").toggleClass("show");
-      $("#despCont").toggleClass("clicked");
-    }
-  });
-
-  $(".login").click(function(){
-      $("#Login").toggleClass("show");
-  });
-
 
   $("#btnCalera").click(function() {
     $(".calera").css("display","block");
@@ -51,49 +32,9 @@ $(document).ready(function(){
     $('.popup').magnificPopup({type:'image'});
 });
 
-function mostrar(){
-    $("nav").toggleClass("show")
-}
-
-function toggleMenu(){
-  $("nav").toggleClass("mostrarMenu")
-}
-
-function menuDesp(mostrar, cambiarEst){
-  $(mostrar).toggleClass("show");
-  $(cambiarEst).css("background","#000000");
-  $(cambiarEst+' a').css("color","#ffffff");
-}
-
 function changeBg(elemento){
     $(elemento).fadeOut(0);
     $(elemento).fadeIn(2200);
-}
-
-function irhacia(id){
-    $('html, body').animate({
-        scrollTop: $(id).offset().top
-    }, 1500);
-}
-
-function about(){
-    irhacia("#about");
-    changeBg("#about");
-}
-
-function tarifas(){
-    irhacia("#tarifas");
-    changeBg("#tarifas");
-}
-
-function pases(){
-    irhacia("#pases");
-    changeBg("#pases");
-}
-
-function horarios(){
-    irhacia("#horarios");
-    changeBg("#horarios");
 }
 
 window.onscroll = function() {arrastrarPagina()};
